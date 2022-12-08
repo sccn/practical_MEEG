5ffadfb8dca42d68bc3847c0ee05ed011be608c6
ramonmc <ram033@eng.ucsd.edu>
2022-12-08 21:56:22 +0100

    Adding loop over session 1 preprocessing and dip loc to generate files for group analysis

--- PracticalMEEG_ERP_Analysis_GroupAnalysis_support.m
+++ PracticalMEEG_ERP_Analysis_GroupAnalysis_support.m
@@ -27,15 +27,97 @@
 % Clearing all is recommended to avoid variable not being erased between calls 
 clear;                                      
 clear globals;
-eeglab;
-
-% We have added the participants.tsv file so we can import as BIDS
-path2data = fullfile(fileparts(pwd), 'Data', 'ds000117-pruned', 'derivatives', 'meg_derivatives');
-path2save = fullfile(fileparts(pwd), 'Data', 'ds000117-pruned', 'derivatives', 'eeglab');
-[STUDY, ALLEEG] = pop_importbids(path2data, 'bidsevent', 'off', 'bidschanloc', 'off', 'outputdir', path2save, 'subjects', [1:6], 'runs', 1)
 
 % Paths to data. Using relative paths so no need to update.
 RootFolder = fileparts(pwd); % Getting root folder
-path2data = fullfile(RootFolder,'Data', 'sub-01') % Path to  the original unzipped data files
-path2save = path2data;                            % Path to save EEGLAB files
-filename = 'sub-01_ses-meg_task-facerecognition_run-01_proc-sss_meg.fif'; % Fif file original name
+path2data = fullfile(RootFolder,'Data', 'ds002718_5_Subjects') % Path to  the original unzipped data files
+
+% Start EEGLAB
+[ALLEEG, EEG, CURRENTSET] = eeglab; 
+
+
+for isubj = 2:6
+    
+    current_path2data = fullfile(path2data,['sub-00', num2str(isubj)],'eeg');
+    current_filename = ['sub-00' num2str(isubj) '_task-FaceRecognition_eeg.set'];
+    current_savefilename = ['sub-00' num2str(isubj) '_task-FaceRecognition_eeg_proc.set'];
+    
+    %% Load data previously imported
+    EEG = pop_loadset('filename', current_filename , 'filepath', current_path2data);
+    
+    %% Remove unwanted channels (61-64)
+    EEG = pop_select(EEG, 'nochannel', [61:64]) ;
+    
+    %% Recomputing head center
+    EEG = pop_chanedit(EEG, 'eval','chans = pop_chancenter( chans, [],[])');
+    
+    %% Re-Reference
+    % Apply Common Average Reference
+    EEG = pop_reref(EEG,[]);
+    
+    %% Resampling
+    % Downsampling to 250 Hz
+    EEG = pop_resample(EEG, 100);
+    
+    %% Filter
+    % Filter the data Highpass at 1 Hz Lowpass at 90Hz (to avoid line noise at 100Hz)
+    EEG = pop_eegfiltnew(EEG, 1, 0);   % High pass at 1Hz
+    EEG = pop_eegfiltnew(EEG, 0, 40);  % Low pass below 40
+    
+    %% Automatic rejection of bad channels
+    % Apply clean_artifacts() to reject bad channels
+    EEG = clean_artifacts(EEG, 'Highpass', 'off',...
+        'ChannelCriterion', 0.9,...
+        'ChannelCriterionMaxBadTime', 0.4,...
+        'LineNoiseCriterion', 4,...
+        'BurstCriterion', 'off',...
+        'WindowCriterion','off' );
+    
+    %% Re-Reference
+    EEG = pop_reref(EEG,[]);
+    
+    % %% Repair bursts and reject bad portions of data
+    EEG = clean_artifacts( EEG, 'Highpass', 'off',...
+        'ChannelCriterion', 'off',...
+        'LineNoiseCriterion', 'off',...
+        'BurstCriterion', 30,...
+        'WindowCriterion',0.3);
+        
+    %% run ICA
+   % EEG = pop_runica(EEG, 'icatype', 'picard', 'maxiter',500,'mode','standard','pca', EEG.nbchan-1);
+    EEG = pop_runica( EEG , 'runica', 'extended',1, 'pca', EEG.nbchan-1);
+    %% automatically classify Independent Components using IC Label
+    EEG  = iclabel(EEG);
+    
+    %% Identify Brain ICs using IC Label classification results
+    [M,I] = max(EEG.etc.ic_classification.ICLabel.classifications,[],2);                       % Use max prob for classification
+    Brain_comps = find(I == find(strcmp(EEG.etc.ic_classification.ICLabel.classes, 'Brain')));
+
+    %% Removing components
+    EEG = pop_subcomp(EEG, Brain_comps,0,1);  
+    
+    %% Extract event-locked trials using events listed in 'eventlist'
+    EEG = pop_epoch( EEG,  {'famous_new' 'famous_second_early' 'famous_second_late'...
+                            'scrambled_new' 'scrambled_second_early' 'scrambled_second_late',...
+                            'unfamiliar_new' 'unfamiliar_second_early' 'unfamiliar_second_late'},...
+                            [-1  2], 'newname', 'wh_S01_allruns_preproc', 'epochinfo', 'yes');
+    
+    %% Perform baseline correction
+    EEG = pop_rmbase(EEG, [-1000 0]);
+
+    %% Clean data by rejecting epochs.
+    [EEG, rejindx] = pop_eegthresh(EEG, 1, 1:EEG.nbchan, -400, 400, EEG.xmin, EEG.xmax, 0, 1);
+    
+    %% Estimate single equivalent current dipoles
+    dipfitpath       = fileparts(which('pop_multifit'));
+    electemplatepath = fullfile(dipfitpath,'standard_BEM/elec/standard_1005.elc');
+    [~,coord_transform] = coregister(EEG.chaninfo.nodatchans, electemplatepath, 'warp', 'auto', 'manual', 'off');
+    EEG = pop_dipfit_settings( EEG, 'hdmfile', fullfile(dipfitpath,'standard_BEM/standard_vol.mat'),...
+                                    'coordformat', 'MNI', 'chanfile', electemplatepath,'coord_transform', coord_transform,...
+                                    'mrifile', fullfile(dipfitpath,'standard_BEM/standard_mri.mat')); 
+    
+    EEG = pop_multifit(EEG, 1:EEG.nbchan,'threshold', 100, 'dipplot','off','plotopt',{'normlen' 'on'});
+
+    %% Save dataset
+    EEG = pop_saveset( EEG,'filename', current_savefilename,'filepath', current_path2data);
+end
